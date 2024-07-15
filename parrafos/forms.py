ba6e2756e2f34da3e00589faa41c7be629cea5b3
jon_hawk <jon.aguero@hawkbiosystems.com>
2024-07-15 09:09:38 +0200

    Ahora con titulo y saltos de linea

--- parrafos/forms.py
+++ parrafos/forms.py
@@ -4,4 +4,8 @@ from .models import Parrafo
 class ParrafoForm(forms.ModelForm):
     class Meta:
         model = Parrafo
-        fields = ['texto']
+        fields = ['titulo', 'texto']
+        widgets = {
+            'titulo': forms.TextInput(attrs={'placeholder': 'Título'}),
+            'texto': forms.Textarea(attrs={'placeholder': 'Escribe tu párrafo aquí...'}),
+        }
