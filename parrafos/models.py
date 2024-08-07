6ea0020f573dd4587c5634022806454ada6f70f7
jon_hawk <jon.aguero@hawkbiosystems.com>
2024-08-07 15:36:39 +0200

    primeros pasos

--- parrafos/models.py
+++ parrafos/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 class Parrafo(models.Model):
     titulo = models.CharField(max_length=200)
-    texto = models.TextField()  # Usar TextField en lugar de CKEditor5Field
+    texto = models.TextField()  # Quill content will be stored as HTML
+    autor = models.CharField(max_length=100, blank=True, null=True)  # Añadir campo autor
     fecha_creacion = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
